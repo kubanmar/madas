10c6a17ec362ce03180124a153713d91da3b3dd7
Martin Kuban <kuban@physik.hu-berlin.de>
2021-08-18 14:35:59 +0200

    WIP implementation of NOMAD API test, skipped as API implementation may change

--- tests/test_NOMAD_enc_API.py
+++ tests/test_NOMAD_enc_API.py
@@ -26,6 +26,249 @@ def test_API(monkeypatch, nomad_test_url):
     test_api = API(nomad_url=nomad_test_url, logger = MockLogger(0), representative_selector = mock_selector)
     return test_api
 
+@pytest.fixture
+def get_material_calculations_response():
+    return {
+                "total_results": 0,
+                "results": [
+                    {
+                    "calc_id": "string",
+                    "upload_id": "string",
+                    "code_name": "string",
+                    "code_version": "string",
+                    "functional_type": "string",
+                    "basis_set_type": "string",
+                    "core_electron_treatment": "string",
+                    "run_type": "string",
+                    "has_dos": True,
+                    "has_band_structure": True,
+                    "has_thermal_properties": True
+                    }
+                ],
+                "representatives": {
+                    "idealized_structure": "string",
+                    "electronic_band_structure": "string",
+                    "electronic_dos": "string",
+                    "thermodynamical_properties": "string"
+                }
+            }
+
+@pytest.fixture
+def search_materials_response():
+    return {
+                "search_by": {
+                    "exclusive": False,
+                    "formula": "TiO2",
+                    "elements": [
+                    "Ti",
+                    "O"
+                    ],
+                    "page": 1,
+                    "per_page": 10,
+                    "restricted": False
+                },
+                "query": "string",
+                "material_type": [
+                    "bulk"
+                ],
+                "material_name": [
+                    "string"
+                ],
+                "structure_type": [
+                    "string"
+                ],
+                "space_group_number": [
+                    0
+                ],
+                "crystal_system": [
+                    "triclinic"
+                ],
+                "band_gap": {
+                    "max": 0,
+                    "min": 0
+                },
+                "has_band_structure": True,
+                "has_dos": True,
+                "has_thermal_properties": True,
+                "functional_type": [
+                    "GGA"
+                ],
+                "basis_set": [
+                    "numeric AOs"
+                ],
+                "code_name": [
+                    "ABINIT"
+                ]
+            }
+
+@pytest.fixture
+def get_material_response():
+    return {
+                "material_id": "string",
+                "formula": "string",
+                "formula_reduced": "string",
+                "material_type": "string",
+                "n_calculations": 0,
+                "has_free_wyckoff_parameters": True,
+                "strukturbericht_designation": "string",
+                "material_name": "string",
+                "bravais_lattice": "string",
+                "crystal_system": "string",
+                "point_group": "string",
+                "space_group_number": 0,
+                "space_group_international_short_symbol": "string",
+                "structure_prototype": "string",
+                "structure_type": "string",
+                "similarity": [
+                    {
+                    "material_id": "string",
+                    "value": 0,
+                    "formula": "string",
+                    "space_group_number": 0
+                    }
+                ]
+            }
+
+@pytest.fixture
+def mock_get_archive_calc_response():
+    return {
+        "section_metadata" : {
+            "encyclopedia" : {
+                "material" : {
+                    "idealized_structure" : {
+                        'atom_labels': ['C'],
+                        'atom_positions': [[0, 0, 0]],
+                        'lattice_vectors': [[1.7760600000000004e-10, 0, 0],
+                        [0, 1.7760600000000004e-10, 0],
+                        [0, 0, 1.7760600000000004e-10]],
+                        'lattice_vectors_primitive': [[1.7760600000000004e-10, 0, 0],
+                        [0, 1.7760600000000004e-10, 0],
+                        [0, 0, 1.7760600000000004e-10]],
+                        'periodicity': [True, True, True],
+                        'number_of_atoms': 1,
+                        'cell_volume': 5.60238434686102e-30,
+                        'wyckoff_sets': [{'wyckoff_letter': 'a', 'indices': [0], 'element': 'C'}],
+                        'lattice_parameters': {'a': 1.7760600000000004e-10,
+                        'b': 1.7760600000000004e-10,
+                        'c': 1.7760600000000004e-10,
+                        'alpha': 1.5707963267948966,
+                        'beta': 1.5707963267948966,
+                        'gamma': 1.5707963267948966}
+                    }
+                },
+                "properties" : {
+                    "energies" : {
+                        "energy_total" : -6.978074033123606e-19
+                    }
+                }
+            }
+        }
+    }
+
+@pytest.fixture
+def get_calculation_response():
+    return {
+        "lattice_parameters": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "alpha": 0,
+            "beta": 0,
+            "gamma": 0
+        },
+        "energies": {
+            "energy_total": 0,
+            "energy_total_T0": 0,
+            "energy_free": 0
+        },
+        "mass_density": 0,
+        "atomic_density": 0,
+        "cell_volume": 0,
+        "wyckoff_sets": {
+            "wyckoff_letter": "string",
+            "indices": [
+            0
+            ],
+            "element": "string",
+            "variables": {
+            "x": 0,
+            "y": 0,
+            "z": 0
+            }
+        },
+        "idealized_structure": {
+            "atom_labels": [
+            "string"
+            ],
+            "atom_positions": [
+            [
+                0
+            ]
+            ],
+            "lattice_vectors": [
+            [
+                0
+            ]
+            ],
+            "lattice_vectors_primitive": [
+            [
+                0
+            ]
+            ],
+            "lattice_parameters": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "alpha": 0,
+            "beta": 0,
+            "gamma": 0
+            },
+            "periodicity": [
+            True
+            ],
+            "number_of_atoms": 0,
+            "cell_volume": 0,
+            "wyckoff_sets": [
+            {
+                "wyckoff_letter": "string",
+                "indices": [
+                0
+                ],
+                "element": "string",
+                "variables": {
+                "x": 0,
+                "y": 0,
+                "z": 0
+                }
+            }
+            ]
+        },
+        "band_gap": 0,
+        "electronic_band_structure": {
+            "reciprocal_cell": [
+            [
+                0
+            ]
+            ],
+            "brillouin_zone": {},
+            "section_k_band_segment": {},
+            "section_band_gap": {}
+        },
+        "electronic_dos": {
+            "dos_energies": [
+            0
+            ],
+            "dos_values": [
+            [
+                0
+            ]
+            ]
+        },
+        "phonon_band_structure": {},
+        "phonon_dos": {},
+        "thermodynamical_properties": {}
+    }
+
 class MockSwaggerClient():
     """
     Mock class to test the responses of the NOMAD API without actually calling it.
@@ -132,29 +375,26 @@ def test_api_calls(test_API, test_response, monkeypatch):
     assert response['test'] == 'answer', "_get_material made wrong request"
 
 @pytest.mark.skip()
-def test_get_calculation(test_API, monkeypatch):
+def test_get_calculation(test_API, 
+                         monkeypatch, 
+                         mock_get_archive_calc_response,
+                         get_material_response, ):
 
     """
-    This test is too much tied to the actual implementation. TODO: add actual data and mock out the responses with this NOMAD data.
-    
-    """
+    This test is not implemented for two reasons:
+        1. It is hard to test the functions not implementation specific, and the implementation may change in the (near) future.
+        2. The API may be adopted to use the NOMAD package instead. Then, the whole structure changes.
+    """    
+
+
+    def mock_get_material(*args, **kwargs):
+        return get_material_response
+
+    monkeypatch.setattr(test_API, '_get_material', mock_get_material)
+    monkeypatch.setattr(test_API, '_get_archive_data', mock_get_archive_calc_response)
+    #monkeypatch.setattr(test_API, '_get_material_properties', mock_get_material_properties)
+    #monkeypatch.setattr(test_API, '_get_calculation_list', mock_get_calculation_list)
 
     # test without providing calculation data
     test_material = test_API.get_calculation("a", "b")
 
-    def mock_get_material():
-        return None
-
-    def mock_get_archive_data():
-        return None
-
-    def mock_get_material_properties():
-        return None
-
-    def mock_get_calculation_list():
-        return None
-
-    monkeypatch.setattr(test_API, '_get_material', mock_get_material)
-    monkeypatch.setattr(test_API, '_get_archive_data', mock_get_archive_data)
-    monkeypatch.setattr(test_API, '_get_material_properties', mock_get_material_properties)
-    monkeypatch.setattr(test_API, '_get_calculation_list', mock_get_calculation_list)
