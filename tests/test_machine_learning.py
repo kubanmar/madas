532155c08a51fbff7b1d4120fe12668890718c04
Martin Kuban <kuban@physik.hu-berlin.de>
2021-10-19 18:46:01 +0200

    fix pickling error for DUMMYFingerprint

--- tests/test_machine_learning.py
+++ tests/test_machine_learning.py
@@ -1,6 +1,12 @@
 import pytest
-from simdatframe.machine_learning import *
+from simdatframe.machine_learning import MatrixMultiKernelLearning
+from simdatframe.fingerprints.DUMMY_fingerprint import DUMMYFingerprint
+from simdatframe import SimilarityMatrix
 
+
+@pytest.fixture
+def similarity_matrix():
+    return None
 
 def test_linear_comb_sim_mat():
     pass
