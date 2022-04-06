79fe3cf54db1ccd1de6a39a20806985ddaa6f6c3
Martin Kuban <kuban@physik.hu-berlin.de>
2022-04-06 14:05:54 +0200

    simple tests for ML

--- tests/test_machine_learning.py
+++ tests/test_machine_learning.py
@@ -1,68 +1,45 @@
 import pytest
 
-#from old.machine_learning_old import MatrixMultiKernelLearning
-from simdatframe.fingerprints.DUMMY_fingerprint import DUMMYFingerprint
-from simdatframe import SimilarityMatrix, fingerprint
+from simdatframe import SimilarityMatrix, Fingerprint
+from simdatframe.machine_learning import SimilarityMatrixRegression
 
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+def target_function(x):
+    return np.exp(-1 * ((x[0] - 0.5)**2 + (x[1] - 0.5)**2) / 0.05)
 
 @pytest.fixture
 def fingerprints():
-    x,y = np.meshgrid(np.round(np.linspace(-1,1, 21), 13), np.round(np.linspace(-1,1, 21), 13))
-    fps = [DUMMYFingerprint.from_list([x_i, y_i]) for x_i, y_i in zip(np.concatenate(x),np.concatenate(y))]
+    from itertools import product
+    fps = [Fingerprint("DUMMY").from_list(arr, target_function = target_function) for arr in product(np.linspace(0,1,num = 20), np.linspace(0,1,num = 20))]    
     return fps
-
-#@pytest.fixture
-#def target_values(fingerprints):
-#    return [fp.y for fp in fingerprints]
 
 @pytest.fixture
 def similarity_matrix(fingerprints):
     return SimilarityMatrix().calculate(fingerprints)
 
-@pytest.fixture
-def predictions():
-    return np.array([0.85212033, 0.65186677, 0.85083564, 1.17246202, 1.45456046,
-       1.3039208 , 1.04890647, 1.81332328, 0.02171413, 0.37266654,
-       0.74205951, 0.80209557, 0.52306346, 0.53255931, 0.40254224,
-       1.81366104, 0.26188152, 1.81407074, 1.0093486 , 0.82097386,
-       0.5821174 , 1.17228633, 0.65259416, 0.82037779, 0.45255129,
-       0.72201384, 0.0529696 , 1.62802466, 0.6135294 , 0.50261377,
-       0.98199621, 0.09353276, 0.7419117 , 0.40334923, 0.25180607,
-       1.1029956 , 0.05416612, 0.10189236, 0.64247004, 0.20189167,
-       0.10468879, 0.34183855, 0.85207326, 1.36555685, 0.50186728,
-       0.80195149, 0.58211389, 0.68175177, 0.09308015, 0.5318232 ,
-       0.10266798, 1.18734279, 0.45286437, 0.26419844, 0.13393568,
-       1.25691   , 0.50293176, 1.28308047, 0.10360016, 0.52272765,
-       0.80196708, 1.06166992, 0.8919839 , 0.32190673, 1.13408093,
-       1.00208927, 0.18271975, 0.41251757, 0.68192247, 1.96669111,
-       0.52192239, 1.13299863, 0.37337646, 0.65198138, 1.167532  ,
-       1.13210155, 1.49688741, 0.13348902, 0.74232435, 0.5821402 ,
-       1.0486623 , 0.40219801, 0.68260358, 0.29265626, 1.28386826,
-       0.49193344, 0.20359403, 0.73051945, 0.64159534, 0.50215567,
-       1.06842983, 1.13169671, 0.53194736, 0.18307647, 1.37571816,
-       0.73183673, 0.25202108, 0.29304008, 0.08200937, 1.0586109 ,
-       0.82069902, 1.27546396, 1.49379033, 0.02278131, 0.97122604,
-       1.11564608, 0.25288028, 0.53314006, 1.62889531, 0.74181255,
-       1.36800575])
+def test_SimilarityMatrixRegression(similarity_matrix, fingerprints):
 
-@pytest.mark.skip
-def test_linear_comb_sim_mat():
-    pass
+    train_set_fps, test_set_fps = train_test_split(fingerprints, random_state=0)
 
-@pytest.mark.skip()
-def test_MatrixMultiKernelLearning(fingerprints, similarity_matrix, predictions):
-    mids, targets = np.transpose(np.array([[fp.mid, fp.y] for fp in fingerprints], dtype = object))
-    np.random.seed(0)
-    mids_train, mids_test, target_train, target_test = train_test_split(mids, targets)
-    train_matrix, test_matrix = similarity_matrix.train_test_split(mids_train, mids_test)
-    mmkl = MatrixMultiKernelLearning(kernel_matrices=[train_matrix], 
-                                 prediction_matrices=[test_matrix], 
-                                 regressor_params={'alpha': 1e-12, 'fit_intercept': True, 'normalize': False})
-    mmkl.fit(target_train)
-    preds = mmkl.predict()
+    train_mids  = [fp.mid for fp in train_set_fps]
+    test_mids  = [fp.mid for fp in test_set_fps]
+    train_y = [fp.y for fp in train_set_fps]
+    test_y = [fp.y for fp in test_set_fps]
 
-    from sklearn.metrics import mean_squared_error
+    train_simat, test_simat = similarity_matrix.train_test_split(train_mids, test_mids)
 
-    assert np.isclose(mean_squared_error(preds, target_test, squared=False), 0.0069, atol=1e-4), "Predictions deviate too much from reference."
+    smr = SimilarityMatrixRegression(train_simat, test_simat)
+
+    smr.fitCV(train_y, regressor_parameters = {'alpha': np.logspace(-1,-12, num = 5)}, show_progress=True, random_seed=0)
+
+    preds = smr.predict()
+    preds_train = smr.predict_fit()
+
+    assert smr.evaluate_test(test_y) <= 1e-4, "Failed prediction"
+
+    assert np.allclose(preds, test_y, atol=5e-2)
+
+    assert mean_squared_error(preds_train, train_y) <= 1e-24, "Failed fit"
