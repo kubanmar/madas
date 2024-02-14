f2b0d3a14a5b22a9ba3b6aae698a29f3209dfb3c
Martin Kuban <kuban@physik.hu-berlin.de>
2024-02-14 16:00:53 +0100

    license

--- madas/utils.py
+++ madas/utils.py
@@ -22,10 +22,6 @@ if is_jupyter:
 else:
     from tqdm import tqdm  # noqa: F401
 
-species_list = 'Vac,H,He,Li,Be,B,C,N,O,F,Ne,Na,Mg,Al,Si,P,S,Cl,Ar,K,Ca,Sc,Ti,V,Cr,Mn,Fe,Co,Ni,Cu,Zn,Ga,Ge,As,Se,Br,Kr,Rb,Sr,Y,Zr,Nb,Mo,Tc,Ru,Rh,Pd,Ag,Cd,In,Sn,Sb,Te,I,Xe,Cs,Ba,La,Ce,Pr,Nd,Pm,Sm,Eu,Gd,Tb,Dy,Ho,Er,Tm,Yb,Lu,Hf,Ta,W,Re,Os,Ir,Pt,Au,Hg,Tl,Pb,Bi,Po,At,Rn,Fr,Ra,Ac,Th,Pa,U,Np,Pu,Am,Cm,Bk,Cf,Es,Fm,Md,No,Lr,Rf,Db,Sg,Bh,Hs,Mt,Ds,Rg,Cn,Nh,Fl,Mc,Lv,Ts,Og'.split(',')
-
-electron_charge = 1.602176565e-19
-
 def rmsle(y_true, y_pred):
     """
     Root means square logarithmic error as used in the NOMAD kaggle competition:
